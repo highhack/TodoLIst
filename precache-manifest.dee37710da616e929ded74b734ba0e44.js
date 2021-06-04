b147f13db6822ccc2e8018db0692aa01b74114ea
highhack <forgreatperson@gmail.com>
2021-06-04 17:10:37 +0300

    Updates

---
+++ precache-manifest.dee37710da616e929ded74b734ba0e44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912b3450545ac325efa8a28444a4abb4",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "5e2d987b6cfeaddaa06b",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "3f1d6389224a34ca0691",
+    "url": "/Todolist/static/js/2.6da09cc4.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.6da09cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2d987b6cfeaddaa06b",
+    "url": "/Todolist/static/js/main.2f656e71.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  },
+  {
+    "revision": "133f50776a6809127ab2ff15ee9a0082",
+    "url": "/Todolist/static/media/sky.133f5077.jpg"
+  },
+  {
+    "revision": "b15035b86deb7c5c693d88ca5473494b",
+    "url": "/Todolist/static/media/todolistBackground.b15035b8.jpg"
+  }
+]);
